8dfc15441e90dd388633a31837c9c20e9fe10da6
Alex Beneteli Ultramari de Lima <alexlima@afpesp.org.br>
2023-09-22 12:08:15 -0300

    Primeiro ponto na bash

---
+++ app.js
@@ -0,0 +1,20 @@
+function toggleMode () {
+    const html = document.querySelector('html')
+    
+    html.classList.toggle('light')
+
+    //Obter TAG img
+    const img = document.querySelector('#profile img')
+
+    //Substituir a img
+    if(html.classList.contains('light')) {
+    //Se light => adicionar imagem
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Foto Mayk Brito sorrindo, óculos escuro, blusa preta, fundo roxo e azul.')
+    } else {
+        //Se sem light => manter imagem atual
+        img.setAttribute('src', './assets/avatar.png')
+    }
+
+}
+
